7355e3fa7a32f4b77ec4e9966ac137fd18d0864b
Circuit Breaker <circuitbreaker437@gmail.com>
2016-05-21 18:22:45 -0400

    Create proto.h
    Version 1 - prototype of functions used in the program

---
+++ file_IO/binary_dump/proto.h
@@ -0,0 +1,8 @@
+/*
+* FILE : proto.h
+* PROGRAMMER : Marcin Czajkowski
+* FIRST VERSION : 2015-11-30
+* DESCRIPTION :
+*	This header contain all functions headers
+*/
+int getSmallFileLenght(const char* filename);
