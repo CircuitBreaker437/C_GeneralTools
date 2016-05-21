671f0995d80bfcf582e147f9ef8c98486994c68f
Circuit Breaker <circuitbreaker437@gmail.com>
2016-05-21 18:21:25 -0400

    Create common.h
    Version 1 - Common libraries for the program

---
+++ file_IO/binary_dump/common.h
@@ -0,0 +1,9 @@
+/*
+* FILE : common.h
+* PROGRAMMER : Marcin Czajkowski
+* FIRST VERSION : 2015-11-30
+* DESCRIPTION :
+*	This header contain neccassary basic headers to run this program
+*/
+#include <stdio.h>
+#include <Windows.h>
